e92dd3713e8643853a560d21d95dec88f4d0c1fe
Anna Wszeborowska <anna.wszeborowska@gmail.com>
2014-09-15 13:48:36 +0200

    added example app to have a reference for creating python bindings

---
+++ example-pyside-cpp/hybridappbinding-cmake/build/hybrid/CMakeFiles/hybrid.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/hybrid.dir/hybrid/hybrid_module_wrapper.cpp.o"
+  "CMakeFiles/hybrid.dir/hybrid/mainwindow_wrapper.cpp.o"
+  "hybrid/hybrid_module_wrapper.cpp"
+  "hybrid/mainwindow_wrapper.cpp"
+  "hybrid.pdb"
+  "hybrid.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/hybrid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ example-pyside-cpp/libhybrid/Main.cpp
@@ -0,0 +1,10 @@
+#include <QtGui>
+#include "MainWindow.h"
+
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.resize(1000, 700);
+    window.show();
+    return app.exec();
+}

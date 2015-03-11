593e16929097bbdd8cce477e900480e94b0563b3
Anna Wszeborowska <anna.wszeborowska@gmail.com>
2015-03-11 22:12:17 +0100

    removed redundant level setting

--- omni3dapp/logger.py
+++ omni3dapp/logger.py
@@ -2,10 +2,8 @@ import logging
 
 LEVEL = logging.DEBUG
 
-
 logging.basicConfig(
     filename='omni3dapp.log',
     format='%(asctime)s - %(module)s:%(funcName)s - %(levelname)s:%(message)s',
     level=LEVEL)
 log = logging.getLogger(__name__)
-log.setLevel(LEVEL)
